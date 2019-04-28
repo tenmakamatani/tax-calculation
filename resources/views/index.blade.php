dcfee222afd7e34e99c2aa5841ae406936149240
鎌谷天馬 <kamatanitenma@KamataniTenma.local>
2019-04-28 22:20:49 +0900

    [update]test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
   <link href="/css/style.css" rel="stylesheet">
 </head>
 <body>
+  <h1>aiueo</h1>
   <section class="taxes">
     <h1 class="taxes__ttl">消費税設定の一覧</h1>
     <div class="taxes__detail">
