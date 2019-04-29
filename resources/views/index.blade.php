c77ec307e3d9ec75abba18a0cf7c719588e09f87
鎌谷天馬 <kamatanitenma@KamataniTenma.local>
2019-04-29 13:17:41 +0900

    [update]make responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,84 +5,75 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>tax-calculation</title>
-  <link href="favicon.ico" rel="shortcut icon">
-  <link href="/css/style.css" rel="stylesheet">
+  <link href="/css/style.css" rel="stylesheet"></link>
+  <link href="/favicon.ico" rel="shortcut icon"></link>
 </head>
 <body>
-  <section class="taxes">
-    <h1 class="taxes__ttl">消費税設定の一覧</h1>
-    <div class="taxes__detail">
-      <div class="taxes__detail__element">
-        <p class="taxes__detail__element__head">開始日付</p>
+  <section class="forms">
+    <div class="forms__form">
+      <h1 class="forms__form__ttl">消費税設定の登録</h1>
+      <form action="/create" method="POST">
+        {{ csrf_field() }}
+        <input type="text" name="date_create" placeholder="開始日付（例：2000-01-01）" class="forms__form__input" autocomplete="off">
+        <input type="text" name="percent" placeholder="税率（例：5）" class="forms__form__input" autocomplete="off">
+        <br>
+        <button type="submit" class="forms__form__btn">登録</button>
+      </form>
+      <div class="forms__form__message">
+        <p class="forms__form__message__error">{{ $errors->first('date_create') }}</p>
+        <p class="forms__form__message__error">{{ $errors->first('percent') }}</p>
       </div>
-      <div class="taxes__detail__element">
-        <p class="taxes__detail__element__head">税率</p>
+    </div>
+    <div class="forms__form">
+      <h1 class="forms__form__ttl">消費税の計算</h1>
+      <form action="/" method="POST">
+        {{ csrf_field() }}
+        <input type="text" name="date_calculate" placeholder="日付（例：2000-01-01）" class="forms__form__input" autocomplete="off">
+        <input type="text" name="money" placeholder="金額（例：10000）" class="forms__form__input" autocomplete="off">
+        <br>
+        <button type="submit" class="forms__form__btn">計算</button>
+      </form>
+      <div class="forms__form__message">
+        <p class="forms__form__message__error">{{ $errors->first('date_calculate') }}</p>
+        <p class="forms__form__message__error">{{ $errors->first('money') }}</p>
       </div>
-      <div class="taxes__detail__element">
-        <p class="taxes__detail__element__head">操作</p>
+      @if ($result != "")
+        <div class="forms__form__result">
+          <p>{{$date_calculate}}</p>
+          <p>{{$result}}</p>
+        </div>
+      @endif
+    </div>
+  </section>
+  <section class="list">
+    <h1 class="list__ttl">消費税設定一覧</h1>
+    <div class="list__children">
+      <div class="list__children__date">
+        <p class="list__children__date__inner">開始日付</p>
+      </div>
+      <div class="list__children__percent">
+        <p class="list__children__percent__inner">税率</p>
+      </div>
+      <div class="list__children__operate">
+        <p class="list__children__operate__inner">操作</p>
       </div>
     </div>
     @foreach ($taxes as $tax)
-      <div class="taxes__detail">
-        <div class="taxes__detail__element">
-          <p class="taxes__detail__element__head">{{$tax->date}}</p>
+      <div class="list__children">
+        <div class="list__children__date">
+          <p class="list__children__date__inner">{{$tax->date}}</p>
         </div>
-        <div class="taxes__detail__element">
-          <p class="taxes__detail__element__head">{{$tax->percent}}</p>
+        <div class="list__children__percent">
+          <p class="list__children__percent__inner">{{$tax->percent}}％</p>
         </div>
-        <div class="taxes__detail__element">
-          <form action="/delete/<?php echo $tax->id; ?>" method="POST">
+        <div class="list__children__delete">
+          <form action="/delete/{{$tax->id}}" method="POST" class="list__children__delete__form">
             {{ csrf_field() }}
-            <button type="submit" class="taxes__detail__element__button">削除</button>
+            <button type="submit" class="list__children__delete__form__button">削除</button>
           </form>
         </div>
       </div>
     @endforeach
   </section>
-  <section class="register">
-    <h1 class="register__ttl">消費税設定の新規登録</h1>
-    <div class="register__form-container">
-      <form action="/create" method="POST" class="register__form-container__form">
-        {{ csrf_field() }}
-        <input type="text" name="date_create" class="register__form-container__form--date" value="{{ old('date_create') }}">
-        <input type="text" name="percent" class="register__form-container__form--percentage" value="{{ old('percent') }}">
-        <span　class="register__container--percent-chara">％</span>
-        <button type="submit" class="register__form-container__form--button">登録</button>
-      </form>
-    </div>
-  </section>
-  <section class="calculate">
-    <h1 class="calculate__ttl">消費税計算</h1>
-    <div class="calculate__form-container">
-      <form action="/" method="POST" class="calculate__form-container__form">
-        {{ csrf_field() }}
-        <input type="text" name="date_calculate" class="calculate__form-container__form--date" value="{{ old('date_calculate') }}">
-        <input type="text" name="money" class="calculate__form-container__form--percentage" value="{{ old('money') }}">
-        <span>円</span>
-        <button type="submit" class="calculate__form-container__form--button">計算</button>
-      </form>
-    </div>
-    <div class="calculate__result-container">
-      <div class="calculate__result-container__result">
-        @if ($result != "")
-          <p class="calculate__result-container__result__inner">{{$result}}</p>
-        @endif
-      </div>
-      <p>円（税込）</p>
-    </div>
-  </section>
-  @if (count($errors) > 0)
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
-  <script type="text/javascript">
-    const inputItem = document.getElementsByTagName("input");
-    for (var i=0; i<inputItem.length; i++){
-      inputItem[i].autocomplete = "off";
-    }
-  </script>
 </body>
 </html>
